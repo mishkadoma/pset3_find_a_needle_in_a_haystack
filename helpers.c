d51fdd02748e8eb0d6f5aa557d8377c6ddd5b419
mishkadoma <mixandoma@ya.ru>
2017-01-26 01:15:03 +0500

    change of search implementation

--- helpers.c
+++ helpers.c
@@ -11,46 +11,46 @@
 
 /**
  * Returns true if value is in array of n values, else false.
- */
-// bool search(int value, int values[], int n)
-// {
-//     if (n<1)
-//       return false;
-//     int start_point = 0;
-//     int center = (start_point+(n-1))/2;
-//     while (center>start_point)
-//     {
-//       center = (start_point+(n-1))/2;
-//       if (value==values[center])
-//       {
-//         return true;
-//       }
-//       else if(value<values[center])
-//       {
-//         n = center;
-//         search(value, values, n);
-//       }
-//       else
-//       {
-//         start_point=center++;
-//         search(value, values, n);
-//       }
-//     }
-//     return false;
-// }
-
+**/
 bool search(int value, int values[], int n)
 {
-  if (n<1)
+    if (n < 1)
+      return false;
+    int start_point = 0;
+    int center = (start_point + n) / 2;
+    while (center >= start_point)
+    {
+      center = (start_point + n) / 2;
+      if (value == values[center])
+      {
+        return true;
+      }
+      else if (value < values[center])
+      {
+        n = center-1;
+        search(value, values, n);
+      }
+      else
+      {
+        start_point = center+1;
+        search(value, values, n);
+      }
+    }
     return false;
-
-  for (int i=0; i<n; i++)
-  {
-    if (value == values[i])
-      return true;
-  }
-  return false;
 }
+//
+// bool search(int value, int values[], int n)
+// {
+//   if (n < 1)
+//     return false;
+//
+//   for (int i = 0; i < n; i++)
+//   {
+//     if (value == values[i])
+//       return true;
+//   }
+//   return false;
+// }
 
 /**
  * Sorts array of n values.
@@ -63,7 +63,7 @@ void sort(int values[], int n)
   while (swap_counter > 0)
   {
     swap_counter = 0;
-    for (int i = 0; i < n - 2; i++)
+    for (int i = 0; i < n-2; i++)
     {
       if (values[i] > values[i+1])
       {
